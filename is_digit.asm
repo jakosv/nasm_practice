3da3c03bd07311e692169e04109bc713dbe1c32d
jakos <jakosvadim@gmail.com>
2022-10-15 17:56:58 +0300

    Split read_num into two modules: read_num and is_digit

---
+++ is_digit.asm
@@ -0,0 +1,23 @@
+%include "procedure.inc"
+
+global is_digit
+
+section .text
+
+; is_digit(char: db): EAX (0 - false, 1 - true)
+is_digit:
+	push ebp
+	mov ebp, esp
+
+	mov eax, [arg(1)]
+	cmp eax, '0'			; if EDX < '0'
+	jl .false			;	return false
+	cmp eax, '9'			; if EDX > '9'
+	jg .false			;	return false
+					; else
+	mov eax, 1			;	return true
+	jmp short .quit
+.false:	xor eax, eax
+.quit:	mov esp, ebp
+	pop ebp
+	ret
--- read_num.asm
+++ read_num.asm
@@ -2,26 +2,9 @@
 %include "procedure.inc"
 
 global read_num
+extern is_digit
 
 section .text
-
-; is_digit(char: db) -> EAX (0 - false, 1 - true)
-is_digit:
-	push ebp
-	mov ebp, esp
-
-	mov eax, [arg(1)]
-	cmp eax, '0'			; if EDX < '0'
-	jl .false			;	return false
-	cmp eax, '9'			; if EDX > '9'
-	jg .false			;	return false
-					; else
-	mov eax, 1			;	return true
-	jmp short .quit
-.false:	xor eax, eax
-.quit:	mov esp, ebp
-	pop ebp
-	ret
 
 ; read_num(number: address): EAX, CL
 ; EAX contains number of characters
